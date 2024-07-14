2e0d59bc4e9b11c9af574045ecad7566df5b9766
snox <snox@MacBook-Air-snox.local>
2024-07-14 03:12:59 +0300

    home main

--- src/components/Home/HomeMain/HomeMain.jsx
+++ src/components/Home/HomeMain/HomeMain.jsx
@@ -6,7 +6,9 @@ const HomeMain = () => {
     return (
         <div className="home__main">
             <div className="home__main-title">NAME</div>
-            <img src={robotImg} alt="robot" className="home__main-robot" />
+            <img src={robotImg} alt="robot" className="home__main-robot"/>
+            <span className="home__elipse purple"></span>
+            <span className="home__elipse orange"></span>
         </div>
     );
 };
