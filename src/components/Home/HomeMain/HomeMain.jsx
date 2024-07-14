3e981dacfa22ba32fece73be6a2bb7d503cb6942
snox <snox@MacBook-Air-snox.local>
2024-07-14 12:38:11 +0300

    home main

--- src/components/Home/HomeMain/HomeMain.jsx
+++ src/components/Home/HomeMain/HomeMain.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './HomeMain.css'
 import robotImg from '../../../assets/Home/robot.png';
+import elemLeft from '../../../assets/Home/elem-left.png';
+import elemRight from '../../../assets/Home/elem-right.png';
 
 const HomeMain = () => {
     return (
@@ -8,7 +10,9 @@ const HomeMain = () => {
             <div className="home__main-title">NAME</div>
             <img src={robotImg} alt="robot" className="home__main-robot"/>
             <span className="home__elipse purple"></span>
+            <img src={elemLeft} alt="" className="left-elem lines"/>
             <span className="home__elipse orange"></span>
+            <img src={elemRight} alt="" className="right-elem lines"/>
         </div>
     );
 };
