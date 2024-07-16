ff4eabc7a773a8b52b7b817005b9169bacfed650
Snox <snox.jz@gmail.com>
2024-07-16 20:55:17 +0300

    home

--- src/components/Home/HomePrompts/HomePrompts.jsx
+++ src/components/Home/HomePrompts/HomePrompts.jsx
@@ -1,43 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Title from "../../ui/Title/Title";
-import "./HomePrompts.css"
+import "./HomePrompts.css";
 import samurai from "../../../assets/Home/samurai.png";
-import Card from "../../Card/Card";
-import Button from "../../ui/Button/Button";
 import tree from "../../../assets/Home/tree.png";
+import "react-multi-carousel/lib/styles.css";
+import PromptsList from "./PromptsList";
+
+
 
 const HomePrompts = () => {
     const [prompts, setPrompts] = useState([
-        {id: 1, image: samurai, title: "Samurai", price: 2.45},
-        {id: 2, image: samurai, title: "Samurai", price: 2.45},
-        {id: 3, image: samurai, title: "Samurai", price: 2.45},
-        {id: 4, image: samurai, title: "Samurai", price: 2.45},
-        {id: 5, image: samurai, title: "Samurai", price: 2.45},
-        {id: 6, image: samurai, title: "Samurai", price: 2.45},
-        {id: 7, image: tree, title: "Samurai", price: 2.453},
-        {id: 8, image: tree, title: "Samurai", price: 2.453},
-        {id: 9, image: tree, title: "Samurai", price: 2.453},
-        {id: 10, image: tree, title: "Samurai", price: 2.453},
-        {id: 11, image: tree, title: "Samurai", price: 2.453},
-        {id: 12, image: tree, title: "Samurai", price: 2.453}
+        { id: 1, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 2, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 3, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 4, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 5, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 6, image: samurai, title: "Samurai", price: 2.45 },
+        { id: 7, image: tree, title: "Samurai", price: 2.453 },
+        { id: 8, image: tree, title: "Samurai", price: 2.453 },
+        { id: 9, image: tree, title: "Samurai", price: 2.453 },
+        { id: 10, image: tree, title: "Samurai", price: 2.453 },
+        { id: 11, image: tree, title: "Samurai", price: 2.453 },
+        { id: 12, image: tree, title: "Samurai", price: 2.453 }
     ]);
+
+
 
     return (
         <div className="home__prompts">
-            <div className="trending__prompts">
+            <div className="prompts">
                 <Title>Trending Prompts</Title>
-                <div className="prompts__list">
-                    <div className="prompts__list-items">
-                        {prompts.map(prompt =>
-                            <Card card={prompt} key={prompt.id}/>
-                        )}
-                    </div>
-                    <Button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="41" viewBox="0 0 22 41" fill="none">
-                            <path d="M1 1L20.5 20.5L1 40" stroke="white" stroke-linecap="round"/>
-                        </svg>
-                    </Button>
-                </div>
+                <PromptsList prompts={prompts}/>
+            </div>
+            <div className="prompts">
+                <Title>Most Popular Prompts This Month</Title>
+                <PromptsList prompts={prompts}/>
+            </div>
+            <div className="prompts">
+                <Title>Most Popular Prompts This Month</Title>
+                <PromptsList prompts={prompts}/>
             </div>
         </div>
     );
--- src/components/Home/HomePrompts/PromptsList.jsx
+++ src/components/Home/HomePrompts/PromptsList.jsx
@@ -1,46 +1,64 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import Card from "../../Card/Card";
-import Button from "../../ui/Button/Button";
+import Carousel from "react-multi-carousel";
+
+const CustomButtonGroup = ({ next, previous }) => {
+    return (
+        <div className="custom-button-group">
+            <button onClick={previous} className="custom-arrow custom-left-arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="41" viewBox="0 0 22 41" fill="none">
+                    <path d="M21 1L1 20.5L21 40" stroke="white" strokeLinecap="round"/>
+                </svg>
+            </button>
+            <button onClick={next} className="custom-arrow custom-right-arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="41" viewBox="0 0 22 41" fill="none">
+                    <path d="M1 1L20.5 20.5L1 40" stroke="white" strokeLinecap="round"/>
+                </svg>
+            </button>
+        </div>
+    );
+};
 
 const PromptsList = ({prompts}) => {
-    const listRef = useRef(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const itemsPerPage = 6;
-
-    const handleScroll = (direction) => {
-        if (direction === 'left' && currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            listRef.current.scrollBy({
-                left: -listRef.current.offsetWidth,
-                behavior: 'smooth'
-            });
-        } else if (direction === 'right' && currentIndex < Math.ceil(prompts.length / itemsPerPage) - 1) {
-            setCurrentIndex(currentIndex + 1);
-            listRef.current.scrollBy({
-                left: listRef.current.offsetWidth,
-                behavior: 'smooth'
-            });
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 1534 },
+            items: 7
+        },
+        largeDesktop: {
+            breakpoint: { max: 1534, min: 1300 },
+            items: 6
+        },
+        desktop: {
+            breakpoint: { max: 1300, min: 1024 },
+            items: 5
+        },
+        miniDesktop: {
+            breakpoint: { max: 1024, min: 768 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 768, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
     };
 
     return (
         <div className="prompts__list">
-            <div className="prompts__list-items" ref={listRef}>
-                {prompts.slice(currentIndex * itemsPerPage, (currentIndex + 1) * itemsPerPage).map(prompt => (
+            <Carousel
+                arrows={false}
+                responsive={responsive}
+                customButtonGroup={<CustomButtonGroup/>}
+                renderButtonGroupOutside={true}
+            >
+                {prompts.map(prompt =>
                     <Card card={prompt} key={prompt.id}/>
-                ))}
-            </div>
-            <Button onClick={() => handleScroll(currentIndex === 0 ? 'right' : 'left')}>
-                {currentIndex === 0 ? (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="41" viewBox="0 0 22 41" fill="none">
-                        <path d="M1 1L20.5 20.5L1 40" stroke="white" stroke-linecap="round"/>
-                    </svg>
-                ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="41" viewBox="0 0 22 41" fill="none">
-                        <path d="M21 1L1.5 20.5L21 40" stroke="white" stroke-linecap="round"/>
-                    </svg>
                 )}
-            </Button>
+            </Carousel>
         </div>
     );
 };
